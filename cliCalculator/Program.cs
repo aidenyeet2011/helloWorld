14c98a15b4f942969c094567732ed1f4e6310932
Code Ninjahttps://github.com/isnowfy/simple.githttps://github.com/isnowfy/simple.git <cnsp@gmail.com>
2023-11-25 12:33:35 -0700

    main program

---
+++ cliCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, World!");
+int l, r;
+try {
+    Console.Write ("please enter top number");
+    l = int.Parse(Console.ReadLine());
+    Console.Write ("please enter bottom number");
+    r = int.Parse (Console.ReadLine());
+    
+    Console.Write("please enter Expressions");
+    string prompt = Console.ReadLine();
+     
+    if (prompt == "+")
+    {
+        var sum = l + r;
+        Console.WriteLine($"{sum}");
+    }
+    else if ()
+    
+}
+catch (FormatException err){
+    Console.WriteLine("Could not parse number");
+}
